f5cfffd98ff9176c288cd9d5673f835510fa36ec
zlatazet <92820527+zlatazet@users.noreply.github.com>
2022-02-15 10:13:13 +0200

    Add files via upload

---
+++ hw2/script.js
@@ -0,0 +1,44 @@
+let number = "23";
+console.log(`number`, number);
+console.log(`typeof number`, typeof number);
+
+var userName = "Lena";
+console.log(`userName`, userName);
+console.log(`typeof userName`, typeof userName);
+
+var userSurname = "Voloshkova";
+console.log(`userSurname`, userSurname);
+console.log(`typeof userSurname`, typeof userSurname);
+
+let user;
+console.log(`typeof user`, typeof user);
+
+let shoppingList;
+console.log(`typeof shoppingList`, typeof shoppingList);
+
+let online = "yes";
+console.log(`typeof online`, typeof online);
+
+const workerSalary = "1000";
+console.log(`workerSalary`, workerSalary);
+console.log(`typeof workerSalary`, typeof workerSalary);
+
+var mouseX = "12";
+var mouseY = "32";
+
+var mouseCoordinates = mouseX + mouseY;
+console.log('typeof mouseCoordinates', typeof mouseCoordinates);
+
+const string = "Hello world!";
+console.log(`string`, string);
+console.log(`typeof string`, typeof string);
+
+const userAge = "12";
+console.log(`userAge`, userAge);
+console.log(`typeof userAge`, typeof userAge);
+
+if (Boolean(userAge)) {
+    console.log("Hello world")
+} else {
+    console.log("bye");
+}
