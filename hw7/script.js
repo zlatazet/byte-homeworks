0789efbf1a5a2ece0af60db592efb4f95d74c5c7
zlatazet <92820527+zlatazet@users.noreply.github.com>
2022-06-06 11:37:04 +0200

    Add files via upload

---
+++ hw7/script.js
@@ -0,0 +1,44 @@
+// Task 1
+
+// const makeYourTiming = () => {
+//     let schedule = {};
+//     let time;
+//     let task;
+//     while (!(time === null) || !(task === null)){
+//         time = prompt(`Enter the time in the format 00:00`);
+//             if (time === null){
+//                 break;
+//             }
+//         task = prompt(`Enter a task`);
+//             if (task === null){
+//                 break;
+//             }
+        
+//         schedule[time] = task;
+       
+        
+//     }  return schedule;
+// }
+// const result = makeYourTiming();
+// console.log(`Schedule: ${result}`);
+
+// Task 2
+
+// const money = {
+//     Mick: 14500,
+//     Per: 12100.35,
+//     Matilda: 2900.40,
+//     Max: 7800.95,
+//     George: 15600,
+//   };
+
+// const sumMoney = () => {
+
+//     let sum = 0;
+//     for (let name of Object.values(money)) {
+//       sum += name;
+//     }
+//     return sum;
+// }
+// const result = sumMoney(money);
+// console.log(`Total: ${result}`);
